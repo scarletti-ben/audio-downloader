49e8f9e8a1dd0d32c3ef2afd3e616ca52458378d
scarletti.ben@gmail.com <scarletti.ben@gmail.com>
2025-01-13 14:07:36 +0000

    Removed issue notes from .bat file

--- mydownloader.bat
+++ mydownloader.bat
@@ -8,10 +8,3 @@ CALL venv\Scripts\activate & venv\Scripts\python.exe main.py %*
 
 @REM Open a Windows explorer window in the downloads directory
 START downloads
-
-@REM Virtual environments still append global system path, causing conflicts
-@REM For production it is advised to clean sys.path in main.py manually
-@REM Example: sys.path = [path for path in sys.path if 'venv' in path]
-@REM THIS STILL DIDN'T FUCKING WORK
-@REM PATH and other things are clearly strung together and you can't clean them all
-@REM retval = subprocess.call(args)
